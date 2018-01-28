241905f31dcfa9100880884f11e55a37cc6bc4f4
Pascal Holy <pascal.holy@pandata.de>
2018-01-28 23:14:58 +0100

    Dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM tiangolo/uwsgi-nginx-flask:python3.6
+
+COPY ./app /app
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
--- app/main.py
+++ app/main.py
@@ -53,4 +53,4 @@ class HelloWorld(Resource):
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True, port=80)
